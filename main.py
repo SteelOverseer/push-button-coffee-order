8bd10832610d12b107b58816b7f005f99a734bfb
Doug Schultz <douglasschultz1@gmail.com>
2025-03-29 18:13:31 -0400

    get dev data

--- main.py
+++ main.py
@@ -1,20 +1,46 @@
 import os
-from terminal_shop import Terminal
 from dotenv import dotenv_values
+from terminal_shop import Terminal
+from terminal_shop.types import Order, OrderCreateResponse, OrderListResponse, OrderGetResponse
+from terminal_shop.types import (
+  Address,
+  AddressCreateResponse,
+  AddressListResponse,
+  AddressDeleteResponse,
+  AddressGetResponse,
+)
 
 config = dotenv_values(".env")
 
 token = config["TERMINAL_BEARER_TOKEN"]
 environment = config["ENVIRONMENT"]
+darkModeID = config["DARK_MODE"]
+darkMode = None
+shipAddress = None
 
-print("Token: ", token)
-print("environment: ", environment)
+client = Terminal(
+  bearer_token=token,  # This is the default and can be omitted
+  environment="dev", # defaults to "production".
+)
 
-# client = Terminal(
-#     bearer_token=os.environ.get("TERMINAL_BEARER_TOKEN"),  # This is the default and can be omitted
-#     # defaults to "production".
-#     environment="dev",
-# )
+try:
+  product = client.product.get(darkModeID)
+  darkMode = product.data
+except:
+  print("Product ErrorError")
 
-# product = client.product.list()
-# print(product.data)
+try:
+  addresses = client.address.list()
+
+  if len(addresses.data) > 0:
+    shipAddress = addresses.data[0]
+  else:
+    print("Address not found")
+except:
+  print("Address Error")
+
+
+print(darkMode)
+print(shipAddress)
+
+# create order needs addressid, card id, varients with qty
